5ec5fbf05b9b29e52d7076e364c8b33385a8d23a
Attila Györffy <attila@attilagyorffy.com>
2019-08-16 11:48:02 +0200

    Initial gem skeleton

---
+++ lib/xhyve/version.rb
@@ -0,0 +1,3 @@
+module Xhyve
+  VERSION = "0.1.0"
+end
---
+++ spec/xhyve_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Xhyve do
+  it "has a version number" do
+    expect(Xhyve::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
